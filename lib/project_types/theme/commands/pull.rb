7992645b7fddb88517573a8f584aec2ca50d7e08
Carmela Leung <carmela.leung@shopify.com>
2020-08-24 17:37:07 -0400

    Fix messages

--- lib/project_types/theme/commands/pull.rb
+++ lib/project_types/theme/commands/pull.rb
@@ -3,24 +3,39 @@ module Theme
   module Commands
     class Pull < ShopifyCli::Command
       options do |parser, flags|
-        parser.on('--password=PASSWORD') { |p| flags[:password] = p }
         parser.on('--store=STORE') { |url| flags[:store] = url }
-        parser.on('--themeid=THEME_ID') { |id| flags[:theme_id] = id }
+        parser.on('--password=PASSWORD') { |p| flags[:password] = p }
+        parser.on('--themeid=THEME_ID') { |id| flags[:themeid] = id }
       end
 
       def call(args, _name)
         form = Forms::Pull.ask(@ctx, args, options.flags)
         return @ctx.puts(self.class.help) if form.nil?
 
+        build(form.store, form.password, form.themeid)
         ShopifyCli::Project.write(@ctx,
                                   project_type: 'theme',
                                   organization_id: nil)
 
-        Themekit.pull(@ctx, store: form.store, password: form.password, themeid: form.theme_id)
+        @ctx.done(@ctx.message('theme.pull.pulled', form.store))
       end
 
       def self.help
-        ShopifyCli::Context.message('theme.pull.help', ShopifyCli::TOOL_NAME, ShopifyCli::TOOL_NAME) # TODO: message
+        ShopifyCli::Context.message('theme.pull.help', ShopifyCli::TOOL_NAME)
+      end
+
+      private
+
+      def build(store, password, themeid)
+        CLI::UI::Frame.open(@ctx.message('theme.checking_themekit')) do
+          Themekit.ensure_themekit_installed(@ctx)
+        end
+
+        CLI::UI::Frame.open(@ctx.message('theme.pull.pull')) do
+          unless Themekit.pull(@ctx, store: store, password: password, themeid: themeid)
+            @ctx.abort(@ctx.message('theme.pull.failed'))
+          end
+        end
       end
     end
   end
--- lib/project_types/theme/forms/pull.rb
+++ lib/project_types/theme/forms/pull.rb
@@ -1,14 +1,20 @@
 module Theme
   module Forms
     class Pull < ShopifyCli::Form
-      flag_arguments :theme_id, :password, :store
+      flag_arguments :themeid, :password, :store
 
-      def ask # TODO: move messages from create to forms
-        self.store ||= CLI::UI::Prompt.ask(ctx.message('theme.forms.create.ask_store'), allow_empty: false)
-        ctx.puts(ctx.message('theme.forms.create.private_app', self.store))
-        self.password ||= CLI::UI::Prompt.ask(ctx.message('theme.forms.create.ask_password'), allow_empty: false)
+      def ask
+        self.store ||= CLI::UI::Prompt.ask(ctx.message('theme.forms.ask_store'), allow_empty: false)
+        ctx.puts(ctx.message('theme.forms.pull.private_app', store))
+        self.password ||= CLI::UI::Prompt.ask(ctx.message('theme.forms.ask_password'), allow_empty: false)
         ctx.system(Themekit::THEMEKIT, "get --list ---store=#{store} --password=#{password}")
-        self.theme_id ||= CLI::UI::Prompt.ask("theme id:") # TODO: change to multiple choice
+        self.themeid ||= CLI::UI::Prompt.ask(ctx.message('theme.forms.pull.ask_theme_id'), allow_empty: false) # TODO: change to multiple choice
+
+        errors = []
+        errors << "store" if store.strip.empty?
+        errors << "password" if password.strip.empty?
+        errors << "theme ID" if themeid.strip.empty?
+        ctx.abort(ctx.message('theme.forms.errors', errors.join(", ").capitalize)) unless errors.empty?
       end
     end
   end
